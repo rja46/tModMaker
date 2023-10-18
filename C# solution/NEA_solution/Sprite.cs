25a1c235caafdaebc3b85482a49074e5f6963690
rja46 <rjanderson15506@gmail.com>
2023-10-18 09:32:35 +0100

    changed file structure

---
+++ C# solution/NEA_solution/Sprite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEA_solution
+{
+    public class Sprite
+    {
+        private string imageLocation;
+        private bool isAnimated;
+        private int frameTime;
+        public int sizeX;
+        public int sizeY;
+        public Sprite()
+        {
+
+        }
+
+        public string get_sprite_path() { return imageLocation; }
+        public void set_sprite_path(string path) { imageLocation = path; }
+    }
+}
